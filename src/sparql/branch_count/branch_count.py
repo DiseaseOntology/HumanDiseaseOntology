ebc48c4e102b7db0b7973d2b3be13d00f01615e4
rctauber <rctauber@gmail.com>
2018-12-05 09:19:13 -0500

    Add branch counts to release process

---
+++ src/sparql/branch_count/branch_count.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import os, subprocess, sys
+
+count_dir = 'src/sparql/branch_count/'
+report_dir = 'build/reports/counts/'
+queries = ['anatomical_entity', 
+		   'cellular_proliferation', 
+		   'genetic', 
+		   'infectious_agent', 
+		   'mental_health', 
+		   'metabolism', 
+		   'physical_disorder', 
+		   'syndrome']
+robot_cmd = 'java -jar build/robot.jar query \
+ --input %s.rdf --tdb true --keep-tdb-mappings true --query %s %s'
+
+def main(args):
+	input_ont = args[1]
+	output = args[2]
+
+	cmd = 'cp %s %s.rdf' % (input_ont, input_ont)
+	subprocess.check_call(cmd, shell=True)
+
+	if not os.path.exists(report_dir):
+		os.makedirs(report_dir)
+
+	for q in queries:
+		query = count_dir + q + '.rq'
+		out = report_dir + q + '.tsv'
+		cmd = robot_cmd % (input_ont, query, out)
+		subprocess.check_call(cmd, shell=True)
+
+	counts = []
+	counts.append('Node\tCount\n')
+	for q in queries:
+		out = report_dir + q + '.tsv'
+		with open(out, 'r') as f:
+			lines = f.readlines()
+			header = lines[0][1:].rstrip('\n')
+			count = lines[1]
+			new_line = '%s\t%s' % (header, count)
+			counts.append(new_line)
+
+	with open(output, 'w') as f:
+		for c in counts:
+			f.write(c)
+
+	cmd = 'rm %s.rdf && rm -rf %s && rm -rf .tdb' % (input_ont, report_dir)
+	subprocess.check_call(cmd, shell=True)
+
+if __name__ == '__main__':
+	main(sys.argv)
